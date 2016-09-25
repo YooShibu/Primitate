ad03447ec6bb6d0d52f5ed87374f5a70e5cd59be
Yohei Shibuya <yoshibu.develop@gmail.com>
2016-09-25 13:10:04 +0900

    time travelers moved to primitate-time-traveler

--- lib/TTPrimitate.d.ts
+++
@@ -1,9 +0,0 @@
-import TimeTraveler from "./TimeTraveler";
-declare function startTTPrimitate<T extends {
-    [key: string]: any;
-}>(initialState: T): {
-    createAction: <U>(pick: (state: T) => U) => <V>(action: (previousState: U, next?: V, initialState?: U) => U) => (next?: V) => U;
-    subscribe: <U>(pick: (state: T) => U) => (listener: (state: U) => void) => () => void;
-    createTimeTraveler: <U>(pick: (state: T) => U) => <V>(memorize: (state: U) => V, remember: (memory: V, state: U) => U) => TimeTraveler<V, U>;
-};
-export default startTTPrimitate;
--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import startPrimitate from "./Primitate.d"
-// Time Travelable Store
-import startTTPrimitate from "./TTPrimitate.d"
-
-export { startPrimitate, startTTPrimitate }
