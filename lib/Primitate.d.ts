ddb8a0b736ae02f84d051c5f0ffdd8fa73fd880e
Yohei Shibuya <yoshibu.develop@gmail.com>
2016-10-02 10:39:58 +0900

    fix subscribe type

--- lib/Primitate.d.ts
+++ lib/Primitate.d.ts
@@ -3,7 +3,7 @@ export declare type Action<NEXT, RESULT> = (next: NEXT) => {
 };
 export declare type action<T> = <U>(action: (prevState: T, next?: U, initialState?: T, stateTree?: T) => T) => Action<U, T>;
 export declare type createAction<T> = <U>(pick: (state: T) => U) => action<U>;
-export declare type subscribe<T> = <U>(pick: (state: T) => U) => (listener: (state: U) => void) => () => void;
+export declare type subscribe<T> = <U>(pick: (state: T) => U) => (listener: (state: T) => void) => () => void;
 declare function startPrimitate<T extends {
     [key: string]: any;
 }>(initialState: T): {
